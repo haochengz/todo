6521685b4c3efda1dd188c0dbbdc2fefbacdfb86
haochengz <haochengzhao@outlook.com>
2018-05-16 21:58:19 +0800

    add a entry point and configure the entry path

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: path()
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    }
 };
