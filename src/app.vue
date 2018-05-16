4605c7bca7555ae2b01ced77aa221ee53adc5b18
haochengz <haochengzhao@outlook.com>
2018-05-16 21:46:08 +0800

    add a new vue component to project

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="text">{{text}}</div>
+</template>
+
+<script>
+    export default {
+        name: "main",
+        data() {
+            return {
+                text: "ABC"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #text{
+        color: purple;
+    }
+</style>
