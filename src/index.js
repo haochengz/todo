47cf934259ebf684484878d09e70a9c085da93c1
haochengz <haochengzhao@outlook.com>
2018-05-16 22:39:54 +0800

    using stylus write css style and using stylus-loader loader stylus file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './app.vue'
 
 import './assets/styles/foo.css'
+import './assets/styles/bar.styl'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
