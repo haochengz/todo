e754c12c3fd9d00f4a95fd20c5edd4f69654e5dc
haochengz <haochengzhao@outlook.com>
2018-05-17 14:37:08 +0800

    lost focus of the index background

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,30 @@
 <template>
-    <div id="text">{{text}}</div>
+    <div id="app">
+        <div id="cover"></div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "main",
-        data() {
-            return {
-                text: "ABCDE"
-            }
-        }
     }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+    #app {
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0
+    };
+    #cover{
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0
+        background-color #999
+        opacity .7
+        z-index -1
+    }
 </style>
